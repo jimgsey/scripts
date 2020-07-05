a583a0aff3cd54fcf87ac1ed5c6814a0b0903a9a
daniml3 <danimoral1001@gmail.com>
2020-07-05 23:39:08 +0000

    linux-setup.sh : install some packages via apt and minor tweaks
    Signed-off-by: daniml3 <danimoral1001@gmail.com>

--- linux-setup.sh
+++ linux-setup.sh
@@ -2,24 +2,13 @@
 echo "Running script as $(whoami)"
 cd ${HOME}
 
-echo "Installing needed packages for running the script..."
-sudo apt install unzip wget -y
-
-echo "Installing Platform Tools..."
-rm -rf platform-tools-latest-linux.zip ${HOME}/bin/platform-tools/
-wget https://dl.google.com/android/repository/platform-tools-latest-linux.zip
-mkdir -p ${HOME}/bin/
-unzip platform-tools-latest-linux.zip -d ${HOME}/bin/
-printf '\n# add Android SDK platform tools to path \nif [ -d "$HOME/platform-tools" ] ; then \n PATH="$HOME/bin/platform-tools:$PATH" \nfi\n' >> ${HOME}/.profile
-echo "Installing packages for building android..."
+echo "Installing needed packages..."
 sudo apt install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev \
   lib32readline-dev lib32z1-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush \
-  rsync schedtool squashfs-tools xsltproc zip zlib1g-dev default-jre default-jdk python python3 -y
+  rsync schedtool squashfs-tools xsltproc zip zlib1g-dev default-jre default-jdk python python3 adb fastboot unzip wget -y
 
 echo "Installing repo..."
-mkdir -p ${HOME}/android/
 sudo su -c 'curl https://storage.googleapis.com/git-repo-downloads/repo > /usr/bin/repo && chmod +x /usr/bin/repo'
-printf '# set PATH so it includes the bin directory if it exists \nif [ -d "$HOME/bin" ] ; then \n    PATH="$HOME/bin:$PATH" \nfi\n' >> ${HOME}/.profile
 
 echo "Configuring git..."
 git config --global user.email "danimoral1001@gmail.com"
@@ -28,7 +17,15 @@ git config --global user.name "daniml3"
 echo "Generating SSH key..."
 ssh-keygen -t rsa -C "danimoral1001@gmail.com"
 cat ${HOME}/.ssh/id_rsa.pub
+export key_added="false"
 read -p "Add the SSH key to your GitHub account and press enter to continue..."
+
+while [[ $key_added != "true" ]]; do
+read -p "Write 'true' to confirm: " key_added
+done
+
+echo "Creating directories..."
+mkdir -p ${HOME}/android/
 
 echo "Cloning some repos and finishing..."
 cd ${HOME}/android
